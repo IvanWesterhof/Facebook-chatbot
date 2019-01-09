25553d36ddf43a47a7c1c9fbe95f3e6e62863d46
Ivan Westerhof <ivan.westerhof@springbokagency.com>
2019-01-09 08:39:40 +0100

    catching parameter

--- app.js
+++ app.js
@@ -227,7 +227,7 @@ function handleDialogFlowAction(sender, action, messages, contexts, parameters)
     switch (action)
   {
       case "specific.function.vacancies":
-       let reply = "first method to catch the parameter: " + `${messages.parameters.jobtitle}` + "seecond method to catch the parameter: " + `${parameters.jobtitle}`;
+       let reply = "first method to catch the parameter: " + `${messages.parameters}` + "seecond method to catch the parameter: " + `${parameters}`;
        sendTextMessage(sender, reply);
       break;
       // for talk.human case
