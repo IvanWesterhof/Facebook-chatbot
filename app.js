1b975738cdcb12fca9a6ada22a5dbda8c80bc938
Ivan Westerhof <ivan.westerhof@springbokagency.com>
2019-01-09 14:27:08 +0100

    typo fix

--- app.js
+++ app.js
@@ -239,7 +239,7 @@ function handleDialogFlowAction(sender, action, messages, contexts, parameters)
          let vacancylist = "";
          vacancies.offers.forEach(function(offer)
           {
-            if (offer.includes(specificJobtitles))
+            if (offer.includes(specificJobtitle))
             {
               vacancylist = vacancylist + offer.title + "\n";
             }
