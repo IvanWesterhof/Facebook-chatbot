74f80eff96c91edba642bd4bdd8f82122f0782c6
Ivan Westerhof <ivan.westerhof@springbokagency.com>
2019-01-09 09:46:13 +0100

    fixes

--- app.js
+++ app.js
@@ -228,7 +228,19 @@ function handleDialogFlowAction(sender, action, messages, contexts, parameters)
   {
       case "specific.function.vacancies":
         console.log('parameters: ', parameters);
-       let reply =  "seecond method to catch the parameter: " + `${parameters.fields.jobtitle.stringValue}`;
+       let reply =  "second method to catch the parameter: " + parameters.fields.jobtitle.stringValue;
+       let specificJobtitle = ${parameters.fields.jobtitle.stringValue}
+       switch (specificJobtitle)
+        {
+          case "Developer":
+
+
+          break;
+          default:
+              //unhandled action, just send back the text
+              handleMessages(messages, sender);
+
+        }
        sendTextMessage(sender, reply);
       break;
       // for talk.human case
